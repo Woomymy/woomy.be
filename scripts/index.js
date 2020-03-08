24a676f72fc4b903164241feb0757edbb18279c3
Matteo <matzar1808@gmail.com>
2020-03-08 16:34:22 +0100

    Projets

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,6 @@
-console.log("Index loaded")
+console.log("Index loaded");
+let hslbutton = document.querySelector("#HSLbutton")
+hslbutton.addEventListener('click', function () {
+    window.location = "https://github.com/woomy4680-exe/detectionhsl"
+})
+
