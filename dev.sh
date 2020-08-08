7c4e5123685f65dba5ad3182739d5e11cb101c9b
Woomy4680-exe <matzar1808@gmail.com>
2020-08-08 14:49:16 +0200

    Base

---
+++ dev.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sass --watch sass/style.scss:styles/style.css -C --style compressed
--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-console.log("Index loaded");
-let hslbutton = document.querySelector("#HSLbutton")
-hslbutton.addEventListener('click', function () {
-    window.location = "https://github.com/woomy4680-exe/detectionhsl"
-})
-
