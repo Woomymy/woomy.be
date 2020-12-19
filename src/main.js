08798febe40a9b56af1c4386c47355ca0cde5c09
Woomy4680-exe <matzar1808@gmail.com>
2020-12-19 14:24:17 +0100

    💄 Change google logo to html logo for WebSiteButton

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 let app = Vue.createApp(App);
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faGoogle, faGithub, faTwitter, faDev, faReddit } from '@fortawesome/free-brands-svg-icons'
+import { faHtml5, faGithub, faTwitter, faDev, faReddit } from '@fortawesome/free-brands-svg-icons'
 import { faHome, faBook } from '@fortawesome/free-solid-svg-icons'
 library.add(faGithub);
 library.add(faHome);
@@ -11,7 +11,7 @@ library.add(faBook);
 library.add(faTwitter);
 library.add(faDev);
 library.add(faReddit);
-library.add(faGoogle);
+library.add(faHtml5);
 app.component('font-awesome-icon', FontAwesomeIcon);
 import './styles/index.css'
 app.mount('#app')
