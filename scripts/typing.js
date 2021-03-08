a1d640bc1d04cff00b8784d0d50f9f3b650ce76c
Woomy4680-exe <matzar1808@gmail.com>
2021-03-08 21:22:19 +0100

    Title: Add Typed.JS

---
+++ scripts/typing.js
@@ -0,0 +1,9 @@
+const options = {
+    strings: ['Plop', 'Bonjour', 'Hello, world'],
+    typeSpeed: 200,
+    loop: true,
+    cursorChar: '',
+    shuffle: true,
+};
+
+const typed = new Typed('#title', options);
