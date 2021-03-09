ff95225fd1952440981704d45ab9f35f69c70dde
Woomy <woomy@woomy.be>
2021-03-09 15:06:11 +0100

    Title: Remove Typed.JS and add a small prompt

--- scripts/typing.js
+++
@@ -1,9 +0,0 @@
-const options = {
-    strings: ['Plop', 'Bonjour', 'Hello, world'],
-    typeSpeed: 200,
-    loop: true,
-    cursorChar: '',
-    shuffle: true,
-};
-
-const typed = new Typed('#title', options);
